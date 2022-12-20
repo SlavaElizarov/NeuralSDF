804393944fc67dac21aefb1e8d60f089c4805caf
Svyatoslav <dukegonzo@ya.ru>
2022-12-20 19:41:24 +0300

    update callbacks

---
+++ callbacks/__init__.py
@@ -0,0 +1,2 @@
+from .data import *
+from .visualization import *
---
+++ callbacks/data.py
@@ -0,0 +1,9 @@
+import pytorch_lightning as pl
+from training.mesh_data_module import SdfDataModule
+from training.sdf_experiment import SdfExperiment
+
+class ResampleCallback(pl.Callback):
+    def on_train_epoch_end(self, trainer: pl.Trainer, model: SdfExperiment):
+        assert trainer.datamodule is not None
+        assert isinstance(trainer.datamodule, SdfDataModule)
+        trainer.datamodule.resample()
